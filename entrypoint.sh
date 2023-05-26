1386cedfc2dab48f2056c201d0d45922a58528af
yetrun <89437145+yetrun@users.noreply.github.com>
2023-05-26 10:26:08 +0800

    entrypoint.sh: 使用 rake db:prepare

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,7 @@
 set -e
 
 export RACK_ENV=production
-
-if [ -f db/sqlite3/production.sqlite3 ]; then
-    bundle exec rake db:migrate
-else
-    bundle exec rake db:setup
-fi
+bundle exec rake db:prepare
 
 # Exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
